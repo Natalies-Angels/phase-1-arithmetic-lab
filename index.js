aa2174895c6bc79c22fd6e2d62c4b93d618f05d3
Natalies-Angels <natalie.shikuku@student.moringaschool.com>
2023-07-10 13:52:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1=2;
+const num2=31;
+let multiply=num1*num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor((Math.random() * 100) + 1);
+console.log(random);
+
+let max =Math.max(5,10,15,20);
+console.log(max);
+
+const num3=40;
+const num4=9;
+let mod=num3%num4;
+console.log(mod);
